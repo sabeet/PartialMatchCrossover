dc0cbf57c3e9de4d8c1cb6d1f92c693be9dd8b22
sabeet <sabeet.other.way@gmail.com>
2020-08-13 14:48:29 -0400

    added JS within HTML page to attempt input processing

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //plan is to grab the CD and replace the IJ with it
 //assume x is default parent
 
-var x = "ABCDEF";
-var y = "GHIJKL";
+var x = document.getElementById("parent1").value; //"abcdef" 
+var y = document.getElementById("parent2").value; //"ghijkl" 
 
 var charLoc1 = 2;
 var charLoc2 = 4;
@@ -13,6 +13,8 @@ function myFunction(a,b){
   var swapSubstance = a.substring(charLoc1,charLoc2);
 
   return begPartOfString + swapSubstance + endPartOfString;
-};
+}
+
+document.getElementById("result").innerHTML = myFunction(x,y);
 
 console.log(myFunction(x,y));
